018b2392e3a39fffc42c0281fc701d7e26daf51a
Amra <amrazenkic21@gmail.com>
2023-09-19 17:43:25 +0200

    Design changes

--- src/Meal.js
+++ src/Meal.js
@@ -1,6 +1,7 @@
 import useFetch from "./useFetch";
 import loadingPic from './img/loading.svg'
 import DisplayList from "./DisplayList";
+import { Link } from "react-router-dom";
 
 const Meal = () => {
     const { error, isPending, data: meals} = useFetch('https://www.themealdb.com/api/json/v1/1/random.php')
@@ -16,11 +17,20 @@ const Meal = () => {
             <div className="mealContainer">
                 <h2>The meal you should cook is <i>{meal.strMeal}</i></h2>
                 <p>Category: {meal.strCategory}</p>
+                <div className="textSpace">
                 <img src={meal.strMealThumb} alt="" srcset="" />
-                <h4>Ingredients:</h4>
-                <DisplayList meal={meal} />
-                <h4>Instructions:</h4>
-                <p>{meal.strInstructions}</p>
+                
+                    <h4>Ingredients:</h4>
+                    <DisplayList meal={meal} />
+                    <h4>Instructions:</h4>
+                    <p>{meal.strInstructions}</p>
+                    <div className="buttonDiv">
+                        <Link to={'/'}><button>Go back to<br />Home Page</button></Link> 
+                    </div>
+                    
+                    <p className="instructions"></p>
+                </div>
+                
             </div>
             }
         </div>
