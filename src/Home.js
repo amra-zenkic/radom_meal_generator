7434b4c6b9c500e6a2653c3cd1a10635322dab9a
Amra <amrazenkic21@gmail.com>
2023-09-19 16:35:04 +0200

    First Commit: Home/css for home

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import kitchenPic from './img/kitchen.svg'
+
+const Home = () => {
+    return (  
+        <div className="homeContent">
+            <div className="homeElements">
+                <h1>Random generate your next meal here!</h1>
+                <button>GENERATE</button>
+            </div>
+            
+        </div>
+
+    );
+}
+ 
+export default Home;
